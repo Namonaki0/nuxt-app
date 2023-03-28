0499f9852bc7ed202a696432fb01a1cef2d6da58
Andre <andreferreira@live.co.uk>
2023-03-28 23:44:37 +0100

    first commit - pages setup

---
+++ pages/products/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>PRODUCTS</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus veritatis quibusdam minima dignissimos, ullam
+            dolore facere quos, vel cum vitae totam modi! Possimus ipsam mollitia, temporibus voluptatibus ipsa explicabo
+            soluta.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus veritatis quibusdam minima dignissimos, ullam
+            dolore facere quos, vel cum vitae totam modi! Possimus ipsam mollitia, temporibus voluptatibus ipsa explicabo
+            soluta.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    font-size: 36px;
+    margin-bottom: 20px;
+}
+
+p {
+    margin: 20px 0;
+}</style>
