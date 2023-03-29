01ad70c8c5aa95b1763558e30d67f6042a253453
Andre <andreferreira@live.co.uk>
2023-03-29 23:43:39 +0100

    route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, eveniet?</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
