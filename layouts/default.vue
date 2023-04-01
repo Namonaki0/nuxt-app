599e0c65258a6c0d56f38031a55d2525155b50b2
Andre <andreferreira@live.co.uk>
2023-04-02 00:04:05 +0100

    navigation and layout

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <nuxt-link to="/">Nuxt App</nuxt-link>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- outputs page content -->
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+<style scoped>
+.router-link-exact-active {
+    color: #12b488
+}
+
+ul {
+    list-style: none;
+}
+
+a {
+    text-decoration: none;
+}
+</style>
